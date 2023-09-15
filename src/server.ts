e0e4218af6fe9eb9ac474e286cfbc48c9b872a6d
joseCorte-exe <josecorte-corte@hotmail.com>
2023-09-15 13:01:10 -0300

    :tada: first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { getVideosRoute } from './routes/get-videos'
+import { uploadVideoRoute } from './routes/upload-video'
+
+const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.register(getVideosRoute)
+app.register(uploadVideoRoute)
+app.register(getAllPromptsRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP server runnig in http://localhost:3333')
+})
