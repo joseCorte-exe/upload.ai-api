9e99ec07906a2fb40c1b57cb1a07e9ddce564a34
joseCorte-exe <josecorte-corte@hotmail.com>
2023-09-15 16:46:18 -0300

    fix to deploy

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,8 @@ app.register(createTranscriptionRoute)
 app.register(generateAICompletionRoute)
 
 app.listen({
-  port: 3333
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
 }).then(() => {
   console.log('HTTP server runnig in http://localhost:3333')
 })
